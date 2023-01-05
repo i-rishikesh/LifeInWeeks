581c86fb73201bd1eaedf138b6bb6d6acd4841f9
Rishikesh <91647089+i-rishikesh@users.noreply.github.com>
2023-01-05 15:20:48 +0530

    Add files via upload

---
+++ LifeinWeeks.js
@@ -0,0 +1,21 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var daysLeft;
+        var weeksLeft;
+        var monthsLeft;
+        
+        daysLeft= (90 - age) * 365;
+        weeksLeft= (90 - age) * 52;
+        monthsLeft= (90 - age) * 12;
+        
+        console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
+        
+        
+        
+    /*************Don't change the code below**********/
+    }
+    
+    lifeInWeeks(89);
